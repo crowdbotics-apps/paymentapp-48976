1bdbd76b72e6c352e16276661a0b3fbf06c2e846
Crowdbotics <team@crowdbotics.com>
2024-08-07 16:20:42 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "paymentapp-48976",
+  url: "https://paymentapp-48976.botics.co",
+  api: "https://paymentapp-48976.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
